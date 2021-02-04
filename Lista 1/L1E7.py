881eaac50e4ca22bb285596dcca47a63753734af
Guilherme Witkosky <69806194+guilherme-witkosky@users.noreply.github.com>
2021-02-04 10:26:00 -0300

    Update and rename L1E7 to L1E7.py

---
+++ Lista 1/L1E7.py
@@ -0,0 +1,6 @@
+#Exercício 7
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário
+
+lado = int(input('Informe o lado do quadrado: '))
+area = (lado*lado)
+print("O dobro da área do quadrado é: ", (area*2))
