43f43aa6e30e3783d897c6d914cf3f4cbf926b73
Vinicius Henrique Bonazzoli Fogaça de Maria <41800809+Viniciushbf@users.noreply.github.com>
2021-02-04 09:51:31 -0300

    Update L1E12.py

--- Lista 1/L1E12.py
+++ Lista 1/L1E12.py
@@ -1,6 +1,6 @@
 #Exercicio
 #Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula:
-(72.7*altura) - 58
+#(72.7*altura) - 58
 altura = float(input("Informe a sua altura:"))
 peso_ideal = (72.7*altura)-58
 print("O peso ideal para sua altura é: %.2f" % peso_ideal)
