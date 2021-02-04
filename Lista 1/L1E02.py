ff46237b66a14dec0a5967435b6260d26153141e
Vinicius Henrique Bonazzoli Fogaça de Maria <41800809+Viniciushbf@users.noreply.github.com>
2021-02-04 10:58:56 -0300

    Realizando o exercício 2 da Lista 2.

---
+++ Lista 1/L1E02.py
@@ -0,0 +1,7 @@
+#Exercício 2 
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+print("DIGITE UM NÚMERO: ")
+numero = input()
+print("====================================================")
+print("NÚMERO INFORMADO = ", numero)
+print("====================================================")
