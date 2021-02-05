028cfdbec79e35e95e129ae2ce4a14a42b4b721f
Vinicius Henrique Bonazzoli Fogaça de Maria <41800809+Viniciushbf@users.noreply.github.com>
2021-02-05 15:10:56 -0300

    Teste realizado | Correção de erros no código.
    Realizado teste no código e foi adicionados ' : ' em alguns if e retirado zero na frente de números.

--- Lista 2/L2E18.py
+++ Lista 2/L2E18.py
@@ -11,25 +11,25 @@ ano = dataSeparada[2]
 
 dataValida = True
 
-if int(mes) == 04 and int(mes) == 06 and int(mes) == 09 and int(mes) == 11 
-    if int(dia) <=0 or int(dia) >30 >:
+if int(mes) == 4 and int(mes) == 6 and int(mes) == 9 and int(mes) == 11: 
+    if int(dia) <=0 or int(dia) >30 :
         dataValida = False
 else:
-    if int(dia) <=0 or int(dia) >31 >:
+    if int(dia) <=0 or int(dia) >31 :
         dataValida = False
 
 
 if int(ano) % 100 != 0 and int(ano) % 4 == 0 or int(ano) % 400 == 0:
     if int(dia) <=0 or int(dia) > 29:
         dataValida = False
-elif int(mes) == 02:
+elif int(mes) == 2:
     if int(dia) <=0 or int(dia) > 28:
         dataValida = False
         
-if int(mes) < 01 and int(mes) > 12
+if int(mes) < 1 and int(mes) > 12:
     dataValida = False
 
-if int(mes) < 1 and int(mes) > 2021
+if int(ano) < 1 and int(ano) > 2021:
     dataValida = False
 
 print("data valida: ", dataValida)
