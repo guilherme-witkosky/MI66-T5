9d357b3f1087f817a35fe72380d4194eb394eb9c
Vinicius Henrique Bonazzoli Fogaça de Maria <41800809+Viniciushbf@users.noreply.github.com>
2021-02-05 15:34:53 -0300

    Update no exercícios
    Alterado o espaçamento.

--- Lista 2/L2E13.py
+++ Lista 2/L2E13.py
@@ -4,11 +4,19 @@
 
 #Exercício 13
 num = input("Infome um numero e o sistema mostrará um dia da semana: ")
-if num is "1":print("Domingo")
-elif num is "2":print("Segunda-Feira")
-elif num is "3":print("Terça-Feira")
-elif num is "4":print("Quarta-Feira")
-elif num is "5":print("Quinta-Feira")
-elif num is "6":print("Sexta-Feira")
-elif num is "7":print("Sábado")
-else:print("Valor invalido!!")
+if num is "1":
+  print("Domingo")
+elif num is "2":
+  print("Segunda-Feira")
+elif num is "3":
+  print("Terça-Feira")
+elif num is "4":
+  print("Quarta-Feira")
+elif num is "5":
+  print("Quinta-Feira")
+elif num is "6":
+  print("Sexta-Feira")
+elif num is "7":
+  print("Sábado")
+else:
+  print("Valor invalido!!")
