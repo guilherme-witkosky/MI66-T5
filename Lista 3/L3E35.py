b56bc5c47576f0c5ff8a5f7763991d1a3b7416e2
Leonardo Pio <53188680+Pio69@users.noreply.github.com>
2021-02-09 11:21:01 -0300

    revisão

--- Lista 3/L3E35.py
+++ Lista 3/L3E35.py
@@ -1,23 +1,21 @@
-#Os números primos possuem várias aplicações dentro da Computação, por exemplo na Criptografia.
-#Um número primo é aquele que é divisível apenas por um e por ele mesmo. 
-#Faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
 
 num = int(input("Informe um numero: "))
 
 i = 0
 while i <= num:
     if i < 2: 
-        print("O numero i não é primo")
+        print("O numero ", i," não é primo")
         
-    if i == 2: 
-        print('O numero é primo')
+    elif i == 2: 
+        print("O numero ", i," é primo")
         
-    if i % 2 == 0: 
-        print('O numero não é primo')
+    elif i % 2 == 0: 
+        print("O numero ", i," não é primo")
     else: 
         for x in range(3, num // 2, 2):
             if num % x == 0:
-                print('O numero não é primo')
+                print("O numero ", i," não é primo")
                 break
         else:
-            print('O numero é primo')
+            print("O numero ", i," é primo")
+    i+=1
