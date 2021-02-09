0af3eb79f72058783296e8a084f5ef42096d2117
Leonardo Pio <53188680+Pio69@users.noreply.github.com>
2021-02-09 11:13:06 -0300

    revisão do código

--- Lista 3/L3E09.py
+++ Lista 3/L3E09.py
@@ -1,19 +1,5 @@
-#Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
-i = 0
-vetNum = [0, 0, 0, 0, 0] 
-soma = 0
-media = 0
-while i < 5:
-    vetNum[i] = input("Digite um numero:")
-    i +=1
-
-i = 0
-while i < 5:
-    soma += float(vetNum[i])
-    media += float(vetNum[i])
-    i +=1
-    
-media /= 5
-    
-print("A soma dos números é:",soma)
-print("A média dos números é:",media)
+num = 1
+while num <= 50:
+    if (num % 2) != 0:
+        print(num)
+    num+=1
