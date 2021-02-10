2d6306b68b7e171f1e51f7732f2433bb136122f2
Guilherme Witkosky <69806194+guilherme-witkosky@users.noreply.github.com>
2021-02-10 14:51:29 -0300

    Create L5E08.py

---
+++ Lista 5/L5E08.py
@@ -0,0 +1,9 @@
+#Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
+#Exercicio 8
+
+num = int(input("Informe um numero inteiro: "))
+
+def exe8(num):
+    print(len(str(num)))
+
+exe8(num)
