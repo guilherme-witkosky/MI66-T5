11aa56de5f33d324ebdba6f37c826e7aaabf68ad
Guilherme Witkosky <69806194+guilherme-witkosky@users.noreply.github.com>
2021-02-10 14:50:57 -0300

    Create L5E03.py

---
+++ Lista 5/L5E03.py
@@ -0,0 +1,7 @@
+#Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+#Exercicio 3 
+
+def exe3(num1, num2, num3):
+    print("A soma do argumentos é:", (num1 + num2 + num3))
+
+exe3(1, 2, 3)
