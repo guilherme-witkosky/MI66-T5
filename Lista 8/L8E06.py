876fb49776a18b8e534ec6dd0d3349671a750d29
Vinicius Henrique Bonazzoli Fogaça de Maria <41800809+Viniciushbf@users.noreply.github.com>
2021-02-11 11:38:56 -0300

    Update L8E06.py

--- Lista 8/L8E06.py
+++ Lista 8/L8E06.py
@@ -1,43 +1,77 @@
+from google.colab import output
+from IPython.display import clear_output
 class Televisao:
-    def __init__(self)
+    def __init__(self):
         self.volume = 0
         self.canal = 0
     def aumentarVolume(self):
-        if self.volume == 100
+        if self.volume == 100:
             print("Volume no Máximo")
         else:
             self.volume += 1
         
-    def aumentarVolume(self):
-        if self.volume == 0
+    def diminuirVolume(self):
+        if self.volume == 0:
             print("Volume no Mínimo")
         else:
             self.volume -= 1
     def aumentarCanal(self):
-        if self.canal == 0
+        if self.canal == 100:
             print("Você chegou no último canal")
         else:
             self.canal += 1
     def diminuirCanal(self):
-        if self.canal == 0
+        if self.canal == 0:
             print("Você chegou no último canal")
         else:
             self.canal -= 1
 
 def printTelevisao(canal,volume):
     print("*------------------------------------+---+--*")
-    print("|                                    |%3d|  |"%(canal))
+    print("|                           Canal    |%3d|  |"%(canal))
     print("|                                    +---+  |")
     print("|                                           |")
     print("|                                           |")
     print("|                                           |")
     print("|                                           |")
     print("| +---+                                     |")
-    print("| |%3d|                                     |"%(volume))
+    print("| |%3d| Volume                              |"%(volume))
     print("*-+---+------------------------------M-^v-O-*")
     print("                 |       |")
     print("                 |       |")
     print("_________________|_______|__________________")
 
 televisao = Televisao()
-printTelevisao(televisao.aumentarCanal(),televisao.aumentarVolume())
+print("Ligando a TV .")
+print("Ligando a TV ..")
+print("Ligando a TV ...")
+print("Ligando a TV ....")
+while True:
+    
+    print("-------------------------------------------")
+    printTelevisao(televisao.canal,televisao.volume)
+    print("*---------------------*")
+    print("| 1 - Aumenta volume  |")
+    print("| 2 - Dimuinir volume |")
+    print("| 3 - Avança Canal    |")
+    print("| 4 - Voltar Canal    |")
+    print("| 5 - Desligar TV     |")
+    print("*---------------------*")
+    opcao = int(input())
+    clear_output()
+
+    if opcao == 5:
+      break
+    elif opcao == 1:
+      televisao.aumentarVolume()
+    elif opcao == 2:
+      televisao.diminuirVolume()
+    elif opcao == 3:
+      televisao.aumentarCanal()
+    elif opcao == 4:
+      televisao.diminuirCanal()
+    else:
+      print("*----------------*")
+      print("| Opção invalida |")
+      print("*----------------*")
+print("TV desligada!!! (O.o)")
