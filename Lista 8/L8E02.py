341078d2872a4384493e6793e5034e936f2feebb
Vinicius Henrique Bonazzoli Fogaça de Maria <41800809+Viniciushbf@users.noreply.github.com>
2021-02-12 10:45:00 -0300

    Create L8E02.py

---
+++ Lista 8/L8E02.py
@@ -0,0 +1,34 @@
+#Classe Quadrado: Crie uma classe que modele um quadrado:
+#Atributos: Tamanho do lado
+#Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
+
+#Exercicio 2
+
+
+
+
+class Quadrado():
+    def __init__(self, x):
+        self.lado = x
+
+    def mudarLado(self, lado):
+        x = novoLado
+        self.lado = novoLado
+
+    def valLado(self, x):
+        self.lado = x
+        print(x)
+
+    def calcArea(self, x):
+        self.lado = x
+        print(x*x)
+
+teste = Quadrado(5)
+print(teste.lado)
+
+teste.mudarLado(4)
+print(teste.lado)
+
+teste.valLado(3)
+
+teste.calcArea(5)
